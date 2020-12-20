9346258c58425d0950e05e4d881427b0d4968695
FancyJesse <jesus_andrade45@yahoo.com>
2020-12-20 13:32:37 -0800

    removed logger in traffic

--- src/Domain/Traffic/Service/AddTrafficService.php
+++ src/Domain/Traffic/Service/AddTrafficService.php
@@ -18,8 +18,6 @@ final class AddTrafficService extends TrafficService
         $userAgent = $request->getHeader('User-Agent')[0];
         $ipAddress = $request->getAttribute('ip_address');
 
-        $this->logger->info("${requestText} | ${ipAddress} | ${userAgent}");
-
         $traffic = $this->trafficRepository->addTraffic($requestText, $userAgent, $ipAddress);
 
         return $traffic;
