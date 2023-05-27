eea32ba3145371da1561e762642acc938d03e32b
idle-user <jesus_andrade45@yahoo.com>
2023-05-27 15:37:44 -0700

    track post body

--- src/Domain/Traffic/Service/AddTrafficService.php
+++ src/Domain/Traffic/Service/AddTrafficService.php
@@ -13,32 +13,39 @@ final class AddTrafficService extends TrafficService
         $domain = $request->getUri()->getHost();
         $requestMethod = $request->getMethod();
         $requestPath = $request->getUri()->getPath();
-        $requestText = "${requestMethod} ${requestPath}";
+        $requestText = $requestMethod . ' ' . $requestPath;
+        $requestBody = $request->getParsedBody();
 
         $userAgent = $request->getHeaderLine('HTTP_USER_AGENT');
 
         if ($request->hasHeader('HTTP_CF_CONNECTING_IP')) {
             $ipAddress = $request->getHeaderLine('HTTP_CF_CONNECTING_IP');
-            $note = 'HTTP_CF_CONNECTING_IP';
+            $requestBody['ipType'] = 'HTTP_CF_CONNECTING_IP';
         } elseif ($request->hasHeader('HTTP_CLIENT_IP')) {
             $ipAddress = $request->getHeaderLine('HTTP_CLIENT_IP');
-            $note = 'HTTP_CLIENT_IP';
+            $requestBody['ipType'] = 'HTTP_CLIENT_IP';
         } elseif ($request->hasHeader('HTTP_X_REAL_IP')) {
             $ipAddress = $request->getHeaderLine('HTTP_X_REAL_IP');
-            $note = 'HTTP_X_REAL_IP';
+            $requestBody['ipType'] = 'HTTP_X_REAL_IP';
         } elseif ($request->hasHeader('X_FORWARDED_FOR')) {
             $ipAddress = $request->getHeaderLine('X_FORWARDED_FOR');
-            $note = 'X_FORWARDED_FOR';
+            $requestBody['ipType'] = 'X_FORWARDED_FOR';
         } elseif ($request->hasHeader('REMOTE_ADDR')) {
             $ipAddress = $request->getHeaderLine('REMOTE_ADDR');
-            $note = 'REMOTE_ADDR';
+            $requestBody['ipType'] = 'REMOTE_ADDR';
         } else {
             $ipAddress = $request->getHeaderLine('HOST');
-            $note = 'HOST';
+            $requestBody['ipType'] = 'HOST';
         }
+
+        $cleanBodyKeys = ['secret' => '', 'password' => '',  'temp_secret' => '', 'auth_token' => '', 'login_token' => '', 'token' => '', 'auth' => ''];
+
+        $requestBody = array_replace($requestBody, array_intersect_key($cleanBodyKeys, $requestBody));
+        $requestBody = json_encode($requestBody);
         
-        $traffic = $this->trafficRepository->addTraffic($domain, $requestText, $userAgent, $ipAddress, null, $note);
+        $traffic = $this->trafficRepository->addTraffic($domain, $requestText, $userAgent, $ipAddress, null, $requestBody);
 
         return $traffic;
     }
+    
 }
